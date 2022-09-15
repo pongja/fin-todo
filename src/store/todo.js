4d60c19f90bd440e5458620cba4c8de213a1dfb2
pongja <58586037+pongja@users.noreply.github.com>
2022-09-15 15:54:43 +0900

    추가

--- src/store/todo.js
+++ src/store/todo.js
@@ -4,42 +4,42 @@ export const todoStore = defineStore( 'todo', {
     state: () => ({
         todos: [],
         filters: {
-            done: 'all',
-            sortBy: 'none'
+          done: 'all',
+          sortBy: 'none'
         }
     }),
     // computed
     getters: {
-        filteredTodos(state) {
-            let filteredTodos = [...state.todos]
-            // 표시
-            if (state.filters.done !== 'all') {
-                filteredTodos = state.todos.filter(todo => {
-                    switch (state.filters.done) {
-                        case 'true':
-                            return todo.done
-                        case 'false':
-                            return !todo.done
-                    }
-                })
-            }
-            // 정렬
-            if (state.filters.sortBy !== 'none'){
-                filteredTodos.sort((a, b) => {
-                    const aTime = new Date(a.createdAt).getTime
-                    const bTime = new Date(b.createdAt).getTime
-                    switch (state.filters.sortBy) {
-                        case 'newest':
-                            return bTime - aTime
-                        case 'oldest':
-                            return aTime - bTime
-                        default:
-                            return 0
-                    }
-                })
-            }
-            return filteredTodos
+      filteredTodos(state){
+        let filteredTodos = [...state.todos]
+        //표시
+        if(state.filters.done !== 'all'){
+           filteredTodos = state.todos.filter(todo =>{
+                switch (state.filters.done) {
+                    case 'true':
+                        return todo.done
+                    case 'false':
+                        return !todo.done
+                }
+            })
         }
+        if(state.filters.sortBy !== 'none') {
+            filteredTodos.sort((a,b)=>{
+                const aTime = new Date(a.createdAt).getTime()
+                const bTime = new Date(b.createdAt).getTime()
+                switch(state.filters.sortBy){
+                    case 'newest':
+                     return bTime - aTime
+                    case 'oldest':
+                     return aTime - bTime
+                     default :
+                     return 0
+                }
+            })
+        }
+        //정렬
+        return filteredTodos
+      }
     },
     // methods
     actions: {
@@ -56,7 +56,7 @@ export const todoStore = defineStore( 'todo', {
             })
         },
         async updateTodo(payload){
-            const { id, title, done, order } = payload
+            const { id,title, done, order } = payload
             await request({
                 method: 'PUT',
                 body: {
@@ -72,26 +72,26 @@ export const todoStore = defineStore( 'todo', {
             await request({
                 method: 'DELETE',
                 id
-            })
-            this.readTodos()
+              })
+              this.readTodos()
         },
-        async reorderTodos({ oldIndex, newIndex }){
-            const todoIds = this.todos.map(todo => todo.id)
-            const oldId = todoIds[oldIndex] // copy
-            todoIds.splice(oldIndex, 1)
-            todoIds.splice(newIndex, 0, oldId)
-            await request({
-                id: 'reorder',
-                method: 'PUT',
-                body: {
-                    todoIds
-                }
-            })
-            this.readTodos()
+        async reorderTodos({oldIndex, newIndex}){
+         const todoIds = this.todos.map(todo=>todo.id)
+         const oldId = todoIds[oldIndex]
+         todoIds.splice(oldIndex, 1)
+         todoIds.splice(newIndex, 0, oldId)
+
+         await request({
+              id: 'reorder',
+              method: 'PUT',
+              body: {
+                todoIds
+              }
+         })
+         this.readTodos()
         }
     }
 })
-
 async function request(options){
     const {method, body, id ='' } = options
     const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${id}`, {
